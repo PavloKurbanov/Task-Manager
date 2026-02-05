9af4c1f50144fd112e460ea497d0518c0c8de046
PavloKurbanov <113994639+PavloKurbanov@users.noreply.github.com>
2026-02-05 14:29:32 +0200

    Add LocalDataTime and DeadLine

--- src/Entities/Task.java
+++ src/Entities/Task.java
@@ -1,17 +1,24 @@
 package Entities;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Task {
 
     private int id;
     private final String title;
     private Status status;
     private final Priority priority;
+    private final LocalDateTime dateTime;
+    private final LocalDateTime deadline;
 
-    public Task(int id, String title, Priority priority) {
+    public Task(int id, String title, Priority priority, LocalDateTime deadline) {
         this.id = id;
         this.title = title;
         this.priority = priority;
+        this.deadline = deadline;
         this.status = Status.NEW;
+        this.dateTime = LocalDateTime.now();
     }
 
     public int getId() {
@@ -38,9 +45,21 @@ public class Task {
         return priority;
     }
 
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public LocalDateTime getDeadline() {
+        return deadline;
+    }
+
     @Override
     public String toString() {
-        return String.format("ID: %-2d | Назва: %-5s | Пріоритет: %-5s | Статус: %-5s",
-                id, title, priority.getPriority(), status.getStatusName());
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+        String formattedDate = dtf.format(dateTime);
+        String formattedDeadline = dtf.format(deadline);
+
+        return String.format("ID: %-2d | Назва: %-5s | Пріоритет: %-5s | Статус: %-5s | Дата створення: %s | Дедлайн: %s ",
+                id, title, priority.getPriority(), status.getStatusName(), formattedDate, formattedDeadline);
     }
 }
