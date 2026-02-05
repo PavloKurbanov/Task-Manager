bc43991ca5e77d48f989c4de591f1ec6a445d11b
PavloKurbanov <113994639+PavloKurbanov@users.noreply.github.com>
2026-02-05 22:54:50 +0200

    Add TimeFormatter utility class for date formatting

---
+++ src/DataTime/TimeFormatter.java
@@ -0,0 +1,16 @@
+package DataTime;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public final class TimeFormatter {
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm");
+
+    private TimeFormatter() {
+        throw new UnsupportedOperationException("Клас корисності");
+    }
+
+    public static String format(LocalDateTime date) {
+        return (date == null) ? "N/A" : date.format(FORMATTER);
+    }
+}
